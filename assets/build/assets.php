754b3b73a28d2723eb64bb16ef13d41d2afb611b
Imran <imranhsayed@gmail.com>
2020-12-27 12:48:49 +0530

    Update hero styles

--- assets/build/assets.php
+++ assets/build/assets.php
@@ -1,1 +1,1 @@
-<?php return array('js/main.js' => array('dependencies' => array('wp-polyfill'), 'version' => '675d5971252bbdaa19b7e0f9d3273632'), 'js/editor.js' => array('dependencies' => array('wp-polyfill'), 'version' => 'd70a3ea5fe7f596e653eb9fdbff2db37'));
+<?php return array('js/main.js' => array('dependencies' => array('wp-polyfill'), 'version' => '136ddb3263622db977d2823e8f7ea5bc'), 'js/editor.js' => array('dependencies' => array('wp-polyfill'), 'version' => 'd70a3ea5fe7f596e653eb9fdbff2db37'));
